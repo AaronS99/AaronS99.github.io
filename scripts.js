976d4bb2a45d65f05aa159d8aa0f579e07d595e1
AaronS99 <astrupp@rhrk.uni-kl.de>
2022-03-02 15:30:06 +0100

    eingabe

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
 var coll = document.getElementsByClassName("collapsible");
 var i;
+var SListe = ["SanPedro", "mare", "Lokii", "coolJojo", "awonga"];
+var EListe = [0, 0, 0, 0, 0];
 
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
@@ -11,4 +13,36 @@ for (i = 0; i < coll.length; i++) {
       content.style.display = "block";
     }
   });
+}
+
+function updateResults() {
+  var P1 = document.getElementById("PONE").value;
+  var P2 = document.getElementById("PTWO").value;
+  var R1 = document.getElementById("RONE").value;
+  var R2 = document.getElementById("RTWO").value;
+  var inhalt = document.getElementById("ResultListe");
+
+
+  inhalt.innerHTML += P1 + " " + R1 +":" + R2 + " " + P2 + "<br>";
+  if (R1 > R2) {
+    counter(P1)
+  }
+  else {
+    counter(P2)
+  }
+
+}
+
+function counter(name) {
+  for (i=0; i<SListe.length; i++) {
+    if (name == SListe[i]) {
+      EListe[i]++;
+    }
+  }
+  var Ausgabe = [SListe[0] + " " + EListe[0], SListe[1] + " " + EListe[1], SListe[2] + " " +EListe[2], SListe[3] + " " +EListe[3], SListe[4] + " " +EListe[4]]
+  var AString = ""
+  for (i=0; i<5; i++) {
+    AString += Ausgabe[i] + "<br>";
+  }
+  document.getElementById("uebersicht").innerHTML = AString;
 }
